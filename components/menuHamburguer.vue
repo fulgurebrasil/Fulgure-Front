9ab0f44cbdf667ffe4293d96925d8c009ff5f118
alinnegsz <alinne.g@aluno.ifsp.edu.br>
2022-06-16 18:37:23 -0300

    first commit

---
+++ components/menuHamburguer.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-container>
+    <div id="menu-bar">
+        <img src="../static/iconFechar.png" @click="mostrarMenu"/>
+
+        <a href="">Sobre nós</a>
+        <hr/>
+        <a href="">Entrar em contato</a>
+        <hr/>
+        <a href="">Reportar problema</a>
+        <hr/>
+    </div>
+    <div id="menu" @click=" mostrarMenu">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+methods:{
+    mostrarMenu(){
+        const menu = document.getElementById("menu-bar")
+        if(menu.style.display === 'block'){
+            menu.style.display = 'none'
+        }else{
+            menu.style.display = 'block'
+        }
+    }
+}
+}
+</script>
+
+<style>
+#menu{
+    width: 27px;
+    height: 27px;
+    cursor: pointer;
+    margin-bottom: 47px;
+    margin-top: 47px;
+    margin-left: -10px;
+}
+#menu div{
+    background-color: black;
+    width: 30px;
+    height: 5px;
+    margin-bottom: 3px;
+}
+#menu-bar{
+    z-index: 3;
+    position: fixed;
+    float: left;
+    background-color: black;
+    color: white;
+    height: 100%;
+    width: 280px;
+    padding-left: 40px;
+    padding-top: 58px;
+    box-shadow: 3px 3px 3px #9b9b9b;
+    left: 0;
+    top: 0;
+    display: none;
+}
+#menu-bar hr{
+    width: 240px;
+    margin-right: 100px;
+}
+#menu-bar a{
+    text-decoration: none;
+    color: white;
+    font-weight: 100;
+}
+#menu-bar img{
+    cursor: pointer;
+    width: 27px;
+    height: 27px;
+    border-radius: 50%;
+    display: block;
+    margin-bottom: 40px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <b-container>
+    <MenuHamburguer class="menu"> </MenuHamburguer>
+    <h3>Cadastrar</h3>
+    <p class="label instrucao">
+      Insira os seus dados para realizar o cadastro.
+    </p>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <p class="label">Nome</p>
+      <b-form-input
+        class="nome input"
+        type="text"
+        placeholder="Seu nome"
+        v-model="form.nome"
+        required
+      ></b-form-input>
+      <p class="label">Email</p>
+      <b-form-input
+        class="email input"
+        type="email"
+        placeholder="example@mail.com"
+        v-model="form.email"
+        required
+      ></b-form-input>
+      <p class="label">Senha</p>
+      <b-form-input
+        class="senha input"
+        type="password"
+        placeholder="••••••••••••••••••"
+        v-model="form.senha"
+        required
+      ></b-form-input>
+      <b-form-checkbox class="check"
+        >Li e aceito os <span>termos de uso</span>.</b-form-checkbox
+      >
+      <b-button class="enviar" type="submit" v-on:click="criaCadastro"
+        >Enviar</b-button
+      >
+      <b-button class="google" type="button">
+        <img src="../static/google.png" />
+        Cadastrar com Google</b-button
+      >
+    </b-form>
+  </b-container>
+</template>
+
+<script>
+import menuHamburguer from '~/components/menuHamburguer.vue'
+export default {
+  components: { menuHamburguer },
+  name: 'IndexPage',
+  head: {
+    title: 'Cadastro',
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap',
+      },
+    ],
+  },
+  data() {
+    return {
+      form: {
+        nome: '',
+        email: '',
+        senha: '',
+      },
+      show: true,
+    }
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(event) {
+      event.preventDefault()
+      this.form.nome = ''
+      this.form.email = ''
+      this.form.senha = null
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    },
+    criaCadastro() {
+      this.$router.push('/')
+    },
+  },
+}
+</script>
+<style>
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+}
+h3 {
+  font-weight: bolder;
+}
+.input,
+.enviar,
+.google,
+h3 {
+  margin-bottom: 24px;
+}
+.enviar,
+.google {
+  height: 44px;
+  font-weight: bold;
+  width: 100%;
+  border: none;
+  border-radius: 6px;
+  box-shadow: 3px 3px 3px #9b9b9b;
+  cursor: pointer;
+}
+.enviar {
+  background-color: black;
+  color: white;
+}
+.google {
+  background-color: white;
+  color: black;
+}
+.google img {
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+}
+.label {
+  margin-bottom: 14px;
+}
+
+.check {
+  color: rgb(129, 129, 129);
+}
+.check span {
+  color: blue;
+}
+.instrucao {
+  margin-bottom: 56px;
+}
+</style>
