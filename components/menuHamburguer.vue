bf86262613ae3a9693bb16d72a1710ef361e60af
fulgure brasil <fulgurebrasil@gmail.com>
2022-09-08 15:39:18 -0300

    alterando menu hambúrguer

--- components/menuHamburguer.vue
+++ components/menuHamburguer.vue
@@ -1,83 +1,88 @@
 <template>
-    <b-container>
-        <div id="menu-bar">
-            <img src="../static/iconFechar.png" @click="mostrarMenu" />
-            <a href="">Sobre nós</a>
-            <hr>
-            <a href="">Entrar em contato</a>
-            <hr>
-            <a href="">Reportar problema</a>
-            <NuxtLink v-bind:to="`/tela-jogar`">Jogar</NuxtLink>
-        </div>
-        <div id="menu" @click="mostrarMenu">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </b-container>
+  <b-container>
+    <div id="menu-bar">
+      <img src="../static/iconFechar.png" @click="mostrarMenu" />
+      <a href="">Sobre nós</a>
+      <hr />
+      <a href="">Entrar em contato</a>
+      <hr />
+      <a href="">Reportar problema</a>
+      <a href="/tela-jogar/" id="jogar">Jogar</a>
+    </div>
+    <div id="menu" @click="mostrarMenu">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </b-container>
 </template>
 
 <script>
 export default {
-    methods: {
-        mostrarMenu() {
-            const menu = document.getElementById("menu-bar")
-            if (menu.style.display === 'block') {
-                menu.style.display = 'none'
-            } else {
-                menu.style.display = 'block'
-            }
-        }
-    }
+  methods: {
+    mostrarMenu() {
+      const menu = document.getElementById('menu-bar')
+      if (menu.style.display === 'block') {
+        menu.style.display = 'none'
+      } else {
+        menu.style.display = 'block'
+      }
+    },
+  },
 }
 </script>
 
 <style>
 #menu {
-    width: 27px;
-    height: 27px;
-    cursor: pointer;
-    margin-bottom: 47px;
-    margin-top: 47px;
-    margin-left: -10px;
+  width: 27px;
+  height: 27px;
+  cursor: pointer;
+  margin-bottom: 47px;
+  margin-top: 47px;
+  margin-left: -10px;
 }
 
 #menu div {
-    background-color: black;
-    width: 30px;
-    height: 5px;
-    margin-bottom: 3px;
-    border-radius: 6px;
+  background-color: black;
+  width: 30px;
+  height: 5px;
+  margin-bottom: 3px;
+  border-radius: 6px;
 }
 
 #menu-bar {
-    z-index: 3;
-    position: fixed;
-    float: left;
-    background-color: black;
-    color: white;
-    height: 100%;
-    width: 280px;
-    padding-left: 40px;
-    padding-top: 58px;
-    box-shadow: 3px 3px 3px #9b9b9b;
-    left: 0;
-    top: 0;
-    display: none;
+  z-index: 3;
+  position: fixed;
+  float: left;
+  background-color: black;
+  color: white;
+  height: 100%;
+  width: 280px;
+  padding-left: 40px;
+  padding-top: 58px;
+  box-shadow: 3px 3px 3px #9b9b9b;
+  left: 0;
+  top: 0;
+  display: none;
 }
 
 #menu-bar a {
-    text-decoration: none;
-    color: white;
-    font-weight: 100;
+  display: block;
+  text-decoration: none;
+  color: white;
+  font-weight: 100;
 }
 
 #menu-bar img {
-    cursor: pointer;
-    width: 27px;
-    height: 27px;
-    border-radius: 50%;
-    display: block;
-    margin-bottom: 40px;
+  cursor: pointer;
+  width: 27px;
+  height: 27px;
+  border-radius: 50%;
+  display: block;
+  margin-bottom: 40px;
+}
+
+#jogar {
+  margin-top: 35px;
 }
 </style>
